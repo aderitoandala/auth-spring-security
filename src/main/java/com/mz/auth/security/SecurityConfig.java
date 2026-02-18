04a3fe42a3c8fc1a47caf671c776bb7abab0b50f
Adérito Andala <aderitoandala@gmail.com>
2026-02-18 23:07:16 +0200

    Encode the password

--- src/main/java/com/mz/auth/security/SecurityConfig.java
+++ src/main/java/com/mz/auth/security/SecurityConfig.java
@@ -13,7 +13,7 @@ public class SecurityConfig{
 
 @Bean
 public UserDetailsService userDetailsService(){
-UserDetails user = User.builder().username("aderito123").password("{noop}1234").build();
+UserDetails user = User.builder().username("aderito123").password("{bcrypt}$2a$12$5yuF5LqAHM7H13sBsrdF8.VBBs56LF8lW62sMgSBoSCc91He7CI3e").build();
 return new InMemoryUserDetailsManager(user);
 }
 
