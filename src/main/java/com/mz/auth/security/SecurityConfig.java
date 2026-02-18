b554c368d6b1dc4b375e324c574cf65924bc76e4
Adérito Andala <aderitoandala@gmail.com>
2026-02-18 23:14:11 +0200

    Specify the Encoder

--- src/main/java/com/mz/auth/security/SecurityConfig.java
+++ src/main/java/com/mz/auth/security/SecurityConfig.java
@@ -5,7 +5,8 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 @Configuration
@@ -13,8 +14,13 @@ public class SecurityConfig{
 
 @Bean
 public UserDetailsService userDetailsService(){
-UserDetails user = User.builder().username("aderito123").password("{bcrypt}$2a$12$5yuF5LqAHM7H13sBsrdF8.VBBs56LF8lW62sMgSBoSCc91He7CI3e").build();
+UserDetails user = User.builder().username("aderito123").password("$2a$12$5yuF5LqAHM7H13sBsrdF8.VBBs56LF8lW62sMgSBoSCc91He7CI3e").build();
 return new InMemoryUserDetailsManager(user);
+}
+
+@Bean
+public PasswordEncoder passwordEncoder(){
+return new BCryptPasswordEncoder();
 }
 
 }
