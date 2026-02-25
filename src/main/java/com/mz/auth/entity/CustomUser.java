037fe8bfd91892b38dccb3d26ab8690f7af2227f
Adérito Andala <aderitoandala@gmail.com>
2026-02-25 09:10:01 +0200

    Refactor the custom user

--- src/main/java/com/mz/auth/entity/CustomUser.java
+++ src/main/java/com/mz/auth/entity/CustomUser.java
@@ -5,18 +5,28 @@ import jakarta.persistence.Table;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.EnumType;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-import java.util.ArrayList;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import com.mz.auth.entity.enums.UserRole;
+import java.util.List;
 import java.util.Collection;
 
 @Entity
 @Table(name="custom_user")
 public class CustomUser implements UserDetails{
 
+public CustomUser(String username,String password,UserRole role){
+this.username=username;
+this. password=password;
+this.role=role;
+}
+
 @Id
-@GeneratedValue(strategy=GenerationType.IDENTITY)
- private Long id;
+@GeneratedValue(strategy=GenerationType.UUID)
+ private String id;
 
 @Column(nullable=false,unique=true)
  private String username;
@@ -24,9 +34,14 @@ public class CustomUser implements UserDetails{
 @Column(nullable=false)
  private String password;
 
+@Column(nullable=false)
+@Enumerated(EnumType.STRING)
+private UserRole role;
+
 @Override
 public Collection<? extends GrantedAuthority> getAuthorities(){
-return new ArrayList<>();
+if(this.role==UserRole.ADMIN) return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"),new SimpleGrantedAuthority("ROLE_USER"));
+else return List.of(new SimpleGrantedAuthority("ROLE_USER"));
 }
 
 @Override
@@ -38,5 +53,32 @@ return username;
 public String getPassword(){
 return password;
 }
+
+public UserRole getRole(){
+return role;
+}
+
+
+   @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+
 
 }
