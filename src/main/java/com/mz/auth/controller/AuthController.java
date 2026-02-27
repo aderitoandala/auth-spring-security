53f2831cd9e32675f7fe7271dc858bb922401546
Adérito Andala <aderitoandala@gmail.com>
2026-02-27 15:32:36 +0200

    defining an endpoint to sign in new users

--- src/main/java/com/mz/auth/controller/AuthController.java
+++ src/main/java/com/mz/auth/controller/AuthController.java
@@ -2,15 +2,35 @@ package com.mz.auth.controller;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+import com.mz.auth.service.CustomUserDetailsService;
+import com.mz.auth.entity.CustomUser;
+import com.mz.auth.dto.RegisterDTO;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController{
 
+private final CustomUserDetailsService userService;
+
+public AuthController(CustomUserDetailsService userService){
+this.userService=userService;
+}
+
 @GetMapping
 public ResponseEntity<String> startSecurity(){
 return ResponseEntity.ok("Getting start with spring security");
 }
+
+@PostMapping("/signIn")
+public ResponseEntity<Void>registerUser(@RequestBody RegisterDTO data){
+userService.registerUser(data);
+return ResponseEntity.status(HttpStatus.CREATED).build();
+}
+
+
 
 }
