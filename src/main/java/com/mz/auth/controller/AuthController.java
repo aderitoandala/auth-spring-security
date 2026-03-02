aa83311c949618e2e451e2e48721fb168572d275
Adérito Andala <aderitoandala@gmail.com>
2026-03-02 19:12:34 +0200

    Creating an endpoint /login with the authentication manager

--- src/main/java/com/mz/auth/controller/AuthController.java
+++ src/main/java/com/mz/auth/controller/AuthController.java
@@ -9,15 +9,22 @@ import org.springframework.http.HttpStatus;
 import com.mz.auth.service.CustomUserDetailsService;
 import com.mz.auth.entity.CustomUser;
 import com.mz.auth.dto.RegisterDTO;
+import com.mz.auth.dto.LoginRequestDTO;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController{
 
 private final CustomUserDetailsService userService;
+private final AuthenticationManager authenticationManager;
 
-public AuthController(CustomUserDetailsService userService){
+
+public AuthController(CustomUserDetailsService userService,AuthenticationManager authenticationManager){
 this.userService=userService;
+this.authenticationManager=authenticationManager;
 }
 
 @GetMapping
@@ -29,6 +36,12 @@ return ResponseEntity.ok("Getting start with spring security");
 public ResponseEntity<Void>registerUser(@RequestBody RegisterDTO data){
 userService.registerUser(data);
 return ResponseEntity.status(HttpStatus.CREATED).build();
+}
+
+@PostMapping("/login")
+public ResponseEntity<Void>login(@RequestBody LoginRequestDTO data){
+authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(data.username(),data.password()));
+return ResponseEntity.ok().build();
 }
 
 
---
+++ src/main/java/com/mz/auth/dto/LoginRequestDTO.java
@@ -0,0 +1,3 @@
+package com.mz.auth.dto;
+
+public record LoginRequestDTO(String username,String password){}
