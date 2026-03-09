81c7f18adfb996c0eb4a9daa7d50ddd4ceb36d1c
Adérito Andala <aderitoandala@gmail.com>
2026-03-09 21:24:51 +0200

    Creating a JWT filter which will intercept the request before the traditional authentication

---
+++ src/main/java/com/mz/auth/security/JwtFilter.java
@@ -0,0 +1,50 @@
+package com.mz.auth.security;
+import com.mz.auth.repository.UserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+
+@Component
+public class JwtFilter extends OncePerRequestFilter{
+
+private final TokenService tokenService;
+private final UserRepository userRepository;
+
+public JwtFilter(TokenService tokenService,UserRepository userRepository){
+this.tokenService=tokenService;
+this.userRepository=userRepository;
+}
+
+@Override
+protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,FilterChain filterChain)throws ServletException,IOException{
+String token = this.extractToken(request);
+
+if(this.tokenService.isTokenValid(token)){
+String username = this.tokenService.getSubject(token);
+UserDetails user = userRepository.findByUsername(username).orElseThrow();
+
+var authentication = new UsernamePasswordAuthenticationToken(user,null,user.getAuthorities());
+SecurityContextHolder.getContext().setAuthentication(authentication);
+}
+filterChain.doFilter(request,response);
+}
+
+
+public String extractToken(HttpServletRequest request){
+var authHeader=request.getHeader("Authorization");
+if(authHeader==null)return null;
+return authHeader.replace("Bearer ","");
+}
+
+
+
+}
