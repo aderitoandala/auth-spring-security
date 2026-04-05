775c30531377c83d07691c1ce537151ee005f322
Adérito Andala <aderitoandala@gmail.com>
2026-04-05 16:35:20 +0200

    Unnecessary imports deleted on cusom user details service

--- src/main/java/com/mz/auth/service/CustomUserDetailsService.java
+++ src/main/java/com/mz/auth/service/CustomUserDetailsService.java
@@ -1,7 +1,6 @@
 package com.mz.auth.service;
 import org.springframework.stereotype.Service;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import com.mz.auth.repository.UserRepository;
@@ -10,8 +9,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import com.mz.auth.entity.enums.UserRole;
 import com.mz.auth.dto.RegisterDTO;
 import com.mz.auth.security.exception.UserAlreadyExistsException;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 
 
 @Service
