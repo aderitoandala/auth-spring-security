abbed65211aba45d55aecee3ea04ce89e42bea02
Adérito Andala <aderitoandala@gmail.com>
2026-04-14 08:09:50 +0200

    I created a new exception that will be thrown if an error occurs during token generation.

---
+++ src/main/java/com/mz/auth/security/exception/TokenGenerationException.java
@@ -0,0 +1,13 @@
+package com.mz.auth.security.exception;
+
+public class TokenGenerationException extends RuntimeException{
+
+public TokenGenerationException(String message){
+super(message);
+}
+
+
+}
+
+
+
