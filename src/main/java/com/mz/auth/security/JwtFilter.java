dd03805eb04bc108669deb8e5e526f02897b3d45
Adérito Andala <aderitoandala@gmail.com>
2026-04-14 08:05:25 +0200

    I adjusted the filter to better extract the token and performed checks in the flow to avoid NPE.

--- src/main/java/com/mz/auth/security/JwtFilter.java
+++ src/main/java/com/mz/auth/security/JwtFilter.java
@@ -9,6 +9,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import java.io.IOException;
 
@@ -28,12 +29,13 @@ this.userRepository=userRepository;
 protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,FilterChain filterChain)throws ServletException,IOException{
 String token = this.extractToken(request);
 
-if(this.tokenService.isTokenValid(token)){
-String username = this.tokenService.getSubject(token);
-UserDetails user = userRepository.findByUsername(username).orElseThrow();
-
-var authentication = new UsernamePasswordAuthenticationToken(user,null,user.getAuthorities());
-SecurityContextHolder.getContext().setAuthentication(authentication);
+if(token!=null){
+String username = this.tokenService.validateToken(token);
+if(username!=null){
+   UserDetails user = userRepository.findByUsername(username).orElseThrow(()-> new UsernameNotFoundException("User not found"));
+   var authentication = new UsernamePasswordAuthenticationToken(user,null,user.getAuthorities());
+   SecurityContextHolder.getContext().setAuthentication(authentication);
+}
 }
 filterChain.doFilter(request,response);
 }
@@ -41,8 +43,10 @@ filterChain.doFilter(request,response);
 
 public String extractToken(HttpServletRequest request){
 var authHeader=request.getHeader("Authorization");
-if(authHeader==null)return null;
-return authHeader.replace("Bearer ","");
+if(authHeader!=null && authHeader.startsWith("Bearer ")){
+	return authHeader.substring(7);
+}
+return null;
 }
 
 
